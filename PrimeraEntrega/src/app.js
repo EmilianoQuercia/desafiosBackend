f64e320c758d37a0157fdbd157af6e4386cfeb4c
EmilianoQuercia <emilianoquercia1@gmail.com>
2022-12-23 16:16:50 -0300

    agregue gitignore

--- PrimeraEntrega/src/app.js
+++ PrimeraEntrega/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import products from './routes/product.router.js'
+import carts from './routes/carts.router.js'
+
 
 const app = express();
 const PORT = 8080;
@@ -8,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/products',products)
-
-
+app.use('/api/carts', carts)
 
 
 app.listen(PORT, ()=>{
